4fffdd7316c1832d8f6286b4cbc515644193c8c1
[Andrew Core] <[adcore89@gmail.com]>
2021-05-09 09:03:19 -0400

    port added for heroku

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const fs = require('fs');
+
 //notes route
-const { notes } = require ('./notes.html');
+const { notes } = require ('./db/db.json');
 
 //instantiate the server
 const app = express()
+
+//port
+const PORT = process.env.PORT || 3001;
+
+// on launch need to get Notes HTML, Load saved notes, listen
+
+
 
 app.get('/api/notes', (req,res)=> {
     res.send('hello');
 });
 
-app.listen(3001, () => {
-    console.log (`API server now on port 3001!`);
+
+// Listener 
+app.listen(PORT, () => {
+    console.log (`API server now on port ${PORT}`);
 });
