2d5a103e9e1223f8cf619c51c58a2d827e08b109
[Andrew Core] <[adcore89@gmail.com]>
2021-05-09 09:56:13 -0400

    post route added

--- server.js
+++ server.js
@@ -7,12 +7,35 @@ const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express()
 
+//middleware 
+app.use(express.urlencoded({ extended: true }));
+//parse incoming data as json 
+app.use(express.json());
+
 // on launch need to get Notes HTML, Load saved notes, listen
 
+//filter notes 
+
+//create notes 
+function createNewNote (body, noteArray) {
+    console.log(body);
+    
+    return body;
+}
+
+// get route 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
 });
 
+//post route
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
 
 // Listener 
 app.listen(PORT, () => {
